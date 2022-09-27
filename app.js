81c1b32893dbe46786eafc9c4ec4846156f47385
Maurice Roach <mroach1993@hotmail.com>
2022-09-27 16:18:28 -0400

    code more concise

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const open = document.querySelector("#open");
+const modal_container = document.querySelector("#modal_container");
+const close = document.querySelector("#close");
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
+// grab info using querySelector and the class
 
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
 
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
